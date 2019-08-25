e0db3ed901e6032d10f4f3e6d9ea5c45a2ae0a4c
Angel <atalavera86@gmail.com>
2019-08-25 08:13:51 +0200

    Layout component set as functional component * The reason is because no status is present

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { Link } from 'react-router-dom';
 
 import Aux from '../Aux/Aux';
 import classes from './Layout.module.css';
 
-class Layout extends Component {
-    render() {
-        return(
-            <Aux>
-                <header className={ classes.Header }>
-                    <ul>
-                        <li><Link to="/">LISTS</Link></li>
-                        <li><Link to="/search">SEARCH</Link></li>
-                    </ul>
-                </header>
-                <main>
-                    { this.props.children }
-                </main>
-            </Aux>
-        );
-    }
-}
+const layout = (props) => (
+    <Aux>
+        <header className={ classes.Header }>
+            <ul>
+                <li><Link to="/">LISTS</Link></li>
+                <li><Link to="/search">SEARCH</Link></li>
+            </ul>
+        </header>
+        <main>
+            { props.children }
+        </main>
+    </Aux>
+);
 
-export default Layout;
+export default layout;
